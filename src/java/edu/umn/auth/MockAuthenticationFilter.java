765a27ac7799db84c87876af1a466bf28d1a405d
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-01-15 16:56:45 -0600

    fixing property setter

--- src/java/edu/umn/auth/MockAuthenticationFilter.java
+++ src/java/edu/umn/auth/MockAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package edu.umn.auth;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
