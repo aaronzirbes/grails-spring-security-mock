c0ce3413ee9c8e0354d0ad5d252cf022ff567fbd
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2013-11-25 14:22:15 -0600

    bumping plugin release version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.dependency.resolution = {
         }
 
         // Plugin Release
-        build(':release:2.1.0') {
+        build(':release:2.2.1', ':rest-client-builder:1.0.3') {
             export = false
         }
 
